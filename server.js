d2ed9766d357543c7af2b811ecfbc62d74f8cbc3
ahmad <eng.ahmed.anwar@gmail.com>
2022-10-11 09:37:17 +1300

    setup server to use json

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express')
-const hbs = require('express-handlebars')
 
-const userRoutes = require('./routes/users')
+const imageRoutes = require('./routes/users')
 
 const server = express()
 
 // Middleware
-server.engine('hbs', hbs.engine({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
-server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
 
 // Routes
-server.use('/', userRoutes)
+server.use('/api/v1/image', imageRoutes)
 
 module.exports = server
